bb6fb51121c276a204bd706d6b3864ec82315bc1
Shais <144804259+ShaisRishon@users.noreply.github.com>
2024-03-05 15:12:58 +0000

    Update index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include("view/header.php"); ?>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                HOME PAGE
+            </div>
+        </div>
+    </div>
+
+<?php include("view/footer.php"); ?>
